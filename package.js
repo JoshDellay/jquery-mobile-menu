ba6143f0f1a9ddfc6e11fdc57854f651c0c55dc9
Josh Dellay <josh.dellay@gmail.com>
2015-01-26 13:12:45 -0800

    Remove test

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'The best jQuery plugin for app look-alike on- and off-canvas menus with sliding submenus for your website and webapp.',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/JoshDellay/jquery-mobile-menu',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -13,9 +13,4 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.0.3.1');
   api.addFiles('joshdellay:jquery-mobile-menu.js');
-});
-
-Package.onTest(function(api) {
-  api.use('joshdellay:jquery-mobile-menu');
-  api.addFiles('joshdellay:jquery-mobile-menu-tests.js');
 });
