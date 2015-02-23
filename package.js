880c93d342801bc3b9c0a79cd77bbd5bbf991c09
Josh Dellay <josh.dellay@gmail.com>
2015-02-23 10:12:18 -0800

    fix typo in css

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'joshdellay:jquery-mobile-menu',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'A jQuery plugin for app look-alike on- and off-canvas menus.',
   // URL to the Git repository containing the source code for this package.
@@ -15,5 +15,5 @@ Package.onUse(function(api) {
   api.use('jquery@1.11.3', 'client');
   api.imply('jquery', 'client');
   api.addFiles('jquery.mobile.menu.js', 'client');
-  api.addFiles('jquery.mmenu.all.js', 'client');
+  api.addFiles('jquery.mmenu.all.css', 'client');
 });
