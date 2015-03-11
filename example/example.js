e5dc3f648eeb8f1e85a4442f73b3f5b8438be173
dealable <effMarkets@gmail.com>
2015-03-11 19:41:59 -0400

    add menu bar, mobile styling, initialization on render

--- example/example.js
+++ example/example.js
@@ -1,52 +1,38 @@
 if (Meteor.isClient) {
   Template.example.events({
     'click #open': function () {
-      openSlider('my-menu', true);
+      $("#my-menu").trigger("open.mm");
     },
     'click #close': function () {
-      closeSlider('my-menu', true);
+      $("#my-menu").trigger("close.mm");
     }
   });
-}
-
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
- 
-openSlider = function (id, fullscreen) {
-    console.log('calling create slider...');
-    var slider = $('#' + id);
-    var fs;
-
-    if (fullscreen) {
-        fs = 'mm-fullscreen mm-white'
-    } else {
-        fs = 'mm-white'
-    }
-
-    //Initlaize jquery mobile-menu (slide up)
-    slider.mmenu({
-        'onClick': {
-            'blockUI': false,
-            'close': true,
-            'preventDefault': false,
-            'setSelected': true
-        },
-        'offCanvas': {
-            'position': 'right',
-            'zposition': 'front'
-        },
-        'classes': fs.toString()
-    }, {
-        'transitionDuration': 100
+  
+  Template.example.rendered = function() {
+    $("#my-menu").mmenu({
+      classes: "mm-white",
+        // mm-white mm-black mm-light
+      header: true,
+      buttonbar: "Buttonbar",
+      counters: true,
+      footer: {
+         footer: {
+            add: true,
+            content: "(c) 2015"
+         }
+      },
+      offCanvas: {
+            position  : "right",
+            zposition : "front"
+         },
+      onClick: {
+          blockUI: false,
+          close: true,
+          preventDefault: false,
+          setSelected: true
+      }
+    },{
+        transitionDuration: 100  // does not seem to work
     });
-
-    slider.trigger('open.mm');
-};
-
-closeSlider = function (id, callback) {
-    var slider = $('#' + id);
-    slider.trigger('close.mm');
-};
+  };
+}
